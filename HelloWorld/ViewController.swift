3df0bb08ea6be09e93d2eed6c80ed1709679b888
Branko Ajzele <ajzelegmail.com>
2015-04-24 18:44:25 +0200

    Added some labels, text input, and counter.

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var instructionLabel: UILabel? = nil
+    @IBOutlet weak var name: UITextField? = nil
+    @IBOutlet weak var greeting: UILabel? = nil
+    @IBOutlet weak var counterLabel: UILabel? = nil
+    var counter: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("-- viewDidLoad --")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         println("-- didReceiveMemoryWarning --")
     }
 
-
+    @IBAction func sayHello() {
+        if name!.text.isEmpty {
+            greeting?.text = "Ups! Your name?"
+        } else {
+            greeting?.text = "Hello " + name!.text
+        }
+        
+        counter++
+        
+        counterLabel?.text = "clicked: \(counter)"
+    }
+    
 }
 
